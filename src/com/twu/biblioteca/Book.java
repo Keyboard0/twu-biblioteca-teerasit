5abd5afddc55635edaaafc49b147d11ce0ce80df
teerasit.b <teerasit41@gmail.com>
2021-10-14 14:32:08 +0700

    2.4 - 0.5 | Clean comment

--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -1,32 +1,19 @@
 package com.twu.biblioteca;
 
 public class Book extends Item{
-//    private String title;
     private String author;
     private int year;
-//    private boolean checkedOut;
 
     public Book(String title, String author, int year, boolean checkedOut) {
         super(title);
-//        this.title = title;
         this.author = author;
         this.year = year;
-//        this.checkedOut = checkedOut;
         this.setCheckout(checkedOut);
     }
-
-
-//    public String getTitle() {
-//        return title;
-//    }
 
     public String getAuthor() {
         return author;
     }
-
-//    public boolean isCheckedOut() {
-//        return checkedOut;
-//    }
 
     public int getYear() {
         return year;
@@ -39,8 +26,4 @@ public class Book extends Item{
             return String.format("%-10s | %-10s | %d", this.getTitle(), this.getAuthor(), this.getYear());
         }
     }
-
-//    public void setCheckedOut(boolean checkedOut) {
-//        this.checkedOut = checkedOut;
-//    }
 }
--- src/com/twu/biblioteca/Movie.java
+++ src/com/twu/biblioteca/Movie.java
@@ -1,27 +1,17 @@
 package com.twu.biblioteca;
 
 public class Movie extends Item{
-    //Movies have a name, year, director and movie rating (from 1-10 or unrated).
-//    private final String title;
     private final int year;
     private final String director;
     private final String rating;
-//    private boolean checkedOut;
 
     public Movie(String title, int year, String director, String rating, boolean checkedOut) {
         super(title);
-//        this.title = title;
         this.year = year;
         this.director = director;
         this.rating = rating;
-//        this.checkedOut = checkedOut;
         this.setCheckout(checkedOut);
     }
-
-//    public String getTitle() {
-//        return title;
-//    }
-
     public int getYear() {
         return year;
     }
@@ -34,10 +24,6 @@ public class Movie extends Item{
         return rating;
     }
 
-//    public boolean isCheckedOut() {
-//        return checkedOut;
-//    }
-
     public String getMovieInfo() {
         if (this.isCheckout()) {
             return "";
@@ -45,8 +31,4 @@ public class Movie extends Item{
             return String.format("%-20s | %d | %-15s | %-10s", this.getTitle(), this.getYear(), this.getDirector(), this.getRating());
         }
     }
-
-//    public void setCheckedOut(boolean checkedOut) {
-//        this.checkedOut = checkedOut;
-//    }
 }
